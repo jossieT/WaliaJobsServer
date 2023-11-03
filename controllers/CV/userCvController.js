57fb2c270c3bc53a030d6c69ca4b5847c36c0403
jossieT <joseteshe2017@gmail.com>
2023-11-03 22:38:18 +0300

    cv controller update

--- controllers/CV/userCvController.js
+++ controllers/CV/userCvController.js
@@ -37,7 +37,7 @@ exports.addNewCv = AysncHandler (async (req, res)=>{
         skills,
         summary,
 
-    } = req.body
+    } = req.body;
 
     const createdUserCv = await UserCV.create({
         achievements: {
@@ -116,3 +116,101 @@ exports.getAllUserCv = AysncHandler (async (req, res)=>{
     })
 })
 
+exports.updateUserCv = AysncHandler(async (req, res)=>{
+    const {
+        achievements,
+        certificationName,
+        issuedBy,
+        issuedDate,
+        interest,
+        languageName,
+        proficiencyLevel,
+        fullName,
+        refJobTitle,
+        companyName,
+        email,
+        phone,
+        institution,
+        eduCity,
+        fieldOfStudy,
+        yearOfGraduation,
+        expJobTitle,
+        employer,
+        expCity,
+        exStartDate,
+        expEndDate,
+        currentlyWorkHere,
+        headerFirstName,
+        headerLastName,
+        headerPosition,
+        headerCity,
+        headerRegion,
+        headerPhone,
+        headerEmail,
+        profilePic,
+        skills,
+        summary,
+
+    } = req.body
+
+    const updatedUserCv = await UserCV.findByIdAndUpdate(req.params.id, {
+        achievements: {
+            achievements
+        },
+        certification: {
+            certificationName,
+            issuedBy,
+            issuedDate,
+        },
+        interest: {
+            interest
+        },
+        language: {
+            languageName,
+            proficiencyLevel,
+        },
+        reference: {
+            fullName,
+            refJobTitle,
+            companyName,
+            email,
+            phone,
+        },
+        education: {
+            institution,
+            eduCity,
+            qualification: "Diploma",
+            fieldOfStudy,
+            yearOfGraduation
+        },
+        experience: {
+            expJobTitle,
+            employer,
+            expCity,
+            exStartDate,
+            expEndDate,
+            currentlyWorkHere
+        },
+        heading: {
+            headerFirstName,
+            headerLastName,
+            headerPosition,
+            headerCity,
+            headerRegion,
+            headerPhone,
+            headerEmail,
+            profilePic
+        },
+        skills: {
+            skills
+        },
+        summary: {
+            summary
+        }
+    })
+    res.status(201).json({
+        status: "success",
+        meassage: "CV Updted successfully",
+        data: updatedUserCv
+    })
+})
