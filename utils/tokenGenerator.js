c2c619424e494dba8098b22ee3ce52d491ac596d
jossieT <joseteshe2017@gmail.com>
2024-01-13 21:38:25 +0300

    error fix on cookie

--- utils/tokenGenerator.js
+++ utils/tokenGenerator.js
@@ -7,8 +7,11 @@ const generateToken = (res, userId) =>{
 
     res.cookie('jwt', token, {
         httpOnly: true,
-        //secure: process.env.NODE_ENV !== 'development',
-        sameSite: 'strict',
+        // secure: process.env.NODE_ENV !== 'development',
+        // sameSite: 'none',
+
+        secure: true,
+        sameSite: 'None',
         maxAge: 5 * 24 * 60 * 60 * 1000
     })
 }
