cc5bb4cb2a7e4b525b2a322136b4733609919409
jossieT <joseteshe2017@gmail.com>
2024-01-17 14:12:24 +0300

    error fix

--- app/app.js
+++ app/app.js
@@ -21,12 +21,11 @@ const app = express();
 //===Middlewares===
 
 app.use(cors({
-  origin: false,
   credentials:true }));
 
 app.use((req, res, next) => {
   // Set headers to allow cross-origin requests
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  //res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', true);
