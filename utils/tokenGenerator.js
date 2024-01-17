75bea0193bcb36d5d0427a5348c4525bf694c9ac
jossieT <joseteshe2017@gmail.com>
2024-01-17 14:44:44 +0300

    error fix

--- utils/tokenGenerator.js
+++ utils/tokenGenerator.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const { none } = require('../middlewares/imageUpload');
 
 const generateToken = (res, userId) =>{
    // const userId = userId;
@@ -6,11 +7,11 @@ const generateToken = (res, userId) =>{
     const token = jwt.sign({ userId }, "anykey", {expiresIn: "5d"});
 
     res.cookie('jwt', token, {
-        httpOnly: true,
+        httpOnly: false,
         //signed: true,
         secure: false,
         //secure: process.env.NODE_ENV !== 'development',
-        //sameSite: 'none',
+        sameSite: 'none',
 
         // sameSite: 'None',
         maxAge: 5 * 24 * 60 * 60 * 1000
