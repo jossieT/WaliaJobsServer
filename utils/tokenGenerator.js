b4944815287a136e9723cb6834de0fa504f36e77
jossieT <joseteshe2017@gmail.com>
2024-01-21 13:29:39 +0300

    update cookie set

--- utils/tokenGenerator.js
+++ utils/tokenGenerator.js
@@ -7,7 +7,7 @@ const generateToken = (res, userId) =>{
     console.log('token', token)
 
     res.cookie('jwt', token, {
-        sameSite: 'none',
+        sameSite: 'lax',
         httpOnly: true,
         secure: true,//process.env.NODE_ENV !== 'development',
         maxAge: 5 * 24 * 60 * 60 * 1000
